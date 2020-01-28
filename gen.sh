55b9bd5ec1881399597544d70b3d7103cde08dd2
jerry <jerry@wjz.im>
2020-01-28 13:15:56 +0800

    update gfw.js

--- gen.sh
+++ gen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
-# wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt
-# wget http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
+# wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -O gfwlist.txt
+# wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt -O chinalist.txt
+# wget http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -O delegated-apnic-latest.txt
 
 ./gfw-pac.py -i gfwlist.txt \
             -f gfw.js \
